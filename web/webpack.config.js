4cf5029b7e33a1a7e076e63667dd1ed38b783c5e
Julie <lerchejulie@gmail.com>
2024-05-22 10:32:42 -0500

    Replaced web folder

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -23,12 +23,9 @@ module.exports = {
     usedExports: true
   },
   entry: {
-    createBooklist: path.resolve(__dirname, 'src', 'pages', 'createBooklist.js'),
-    viewBooklist: path.resolve(__dirname, 'src', 'pages', 'viewBooklist.js'),
-    searchBooklists: path.resolve(__dirname, 'src', 'pages', 'searchBooklists.js'),
-    viewUserBooklists: path.resolve(__dirname, 'src', 'pages', 'viewUserBooklists.js'),
-    searchBooks: path.resolve(__dirname, 'src', 'pages', 'searchBooks.js'),
-    updateBook: path.resolve(__dirname, 'src', 'pages', 'updateBook.js')
+    createPlaylist: path.resolve(__dirname, 'src', 'pages', 'createPlaylist.js'),
+    viewPlaylist: path.resolve(__dirname, 'src', 'pages', 'viewPlaylist.js'),
+    searchPlaylists: path.resolve(__dirname, 'src', 'pages', 'searchPlaylists.js'),
   },
   output: {
     path: path.resolve(__dirname, 'build', 'assets'),
