e74933dcbedd3bc924bcfabba5d3235a95c02eff
Julie <lerchejulie@gmail.com>
2024-06-07 12:15:10 -0500

    merging

---
+++ TaskWarriorServiceLambda/src/test/java/julielerche/capstone/activity/CreateEncounterActivityTest.java
@@ -0,0 +1,10 @@
+package julielerche.capstone.activity;
+
+import org.mockito.Mock;
+
+public class CreateEncounterActivityTest {
+    @Mock
+    private EncounterDao encounterDao;
+
+    private CreateEncounterActivity createEncounterActivity;
+}
